686345d2ff897061c10452a988eed83309e82b64
rip-IMRC <somen@hotmail.com>
2024-10-27 11:20:14 -0400

    initial commit

---
+++ First.py
@@ -0,0 +1,11 @@
+print("Hi I am Rishi")
+print("This is my first class with Codingal")
+print("How many continents are in the world?")
+print("a. 6")
+print("b. 7")
+print("c. 8")
+choice = input("enter a, b, or c")
+if choice == "b":
+    print("You are correct")
+else:
+    print("Sorry, you are incorrect")
