883b184d589f27294bae5fc0c91d193146e86711
rip-IMRC <somen@hotmail.com>
2024-11-05 19:56:46 -0500

    commit

---
+++ birthday.py
@@ -0,0 +1,6 @@
+birthday1 = input("Enter Joe's birthday in dd/mm/yyyy format:")
+birthday2 = input("Enter Ben's birthday in dd/mm/yyyy format:")
+birthday3 = input("Enter Rishi's birthday in dd/mm/yyyy format:")
+print("Joe's birthday is on",birthday1)
+print("Ben's birthday is on",birthday2)
+print("Rishi's birthday is on",birthday3)
