40c4e927f2909d594d9a4efdc275f0cb058ad19d
rip-IMRC <somen@hotmail.com>
2024-12-22 09:39:49 -0500

    commit

---
+++ weather.py
@@ -0,0 +1,5 @@
+weather=int(input("Enter the weather outside in Farenheit: "))
+if(weather>32):
+    print("You do not need to wear a jacket today, yippee!")
+else:
+    print("You need to wear a jacket today sadly.")
