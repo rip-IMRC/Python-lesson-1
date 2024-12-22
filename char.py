085b7dac78b4b2451f646028ee34103bbec57a40
rip-IMRC <somen@hotmail.com>
2024-12-22 11:11:09 -0500

    commit

---
+++ char.py
@@ -0,0 +1,5 @@
+char=str(input("Enter a single character: "))
+if char=="a" or char=="b" or char=="c" or char=="d" or char=="e" or char=="f" or char=="g" or char=="h" or char=="i" or char=="j" or char=="k" or char=="l" or char=="m" or char=="n" or char=="o" or char=="p" or char=="q" or char=="r" or char=="s" or char=="t" or char=="u" or char=="v" or char=="w" or char=="x" or char=="y" or char=="z" :
+    print("small letter")
+else:
+    print("capital letter")
